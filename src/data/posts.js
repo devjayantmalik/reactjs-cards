a39fb4ecef7c07a377cfe8688d565ef335f6d893
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-01 11:32:44 +0530

    added static data

---
+++ src/data/posts.js
@@ -0,0 +1,106 @@
+export default [
+	{
+		"username": "Abraham Lincoln",
+		"id": 1,
+		"title": "Learning React? Start Small.",
+		"description": "A Fox and a Leopard, resting lazily after a generous dinner, amused themselves by disputing about their good looks.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 20,
+			"comments": 3,
+			"visited": 342
+		}
+	},
+	{
+		"username": "Abraham Lincoln",
+		"id": 2,
+		"title": "Learning Angular? Start Small.",
+		"description": "Long ago, the mice had a general council to consider what measures they could take to outwit their common enemy, the Cat.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 0,
+			"comments": 0,
+			"visited": 2
+		}
+	},
+	{
+		"username": "Albert Einstein",
+		"id": 3,
+		"title": "Learning C++? Practice Here.",
+		"description": "That evening the Wolf entered the fold with the flock. But it happened that the Shepherd took a fancy for mutton broth.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 244,
+			"comments": 3,
+			"visited": 342
+		}
+	},
+	{
+		"username": "Albert Einstein",
+		"id": 4,
+		"title": "Master CSS fundamentals.",
+		"description": "That evening the Wolf entered the fold with the flock. But it happened that the Shepherd took a fancy for mutton broth.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 60,
+			"comments": 87,
+			"visited": 322
+		}
+	},
+	{
+		"username": "Albert Schweitzer",
+		"id": 5,
+		"title": "What the hell is FlexBox?",
+		"description": "A Fox and a Leopard, resting lazily after a generous dinner, amused themselves by disputing about their good looks.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 9,
+			"comments": 5,
+			"visited": 34
+		}
+	},
+	{
+		"username": "Abraham Lincoln",
+		"id": 6,
+		"title": "Happy life style of Heroku.",
+		"description": "Long ago, the mice had a general council to consider what measures they could take to outwit their common enemy, the Cat.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 420,
+			"comments": 9323,
+			"visited": 10000
+		}
+	},
+	{
+		"username": "Alexander Pope",
+		"id": 7,
+		"title": "Retired Frameworks at a glance!",
+		"description": "That evening the Wolf entered the fold with the flock. But it happened that the Shepherd took a fancy for mutton broth.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 0,
+			"comments": 9,
+			"visited": 10
+		}
+	},
+	{
+		"username": "Alexander Pope",
+		"id": 8,
+		"title": "This is really mega Course!",
+		"description": "That evening the Wolf entered the fold with the flock. But it happened that the Shepherd took a fancy for mutton broth.",
+		"url": "https://google.com",
+		"image": "https://picsum.photos/700/400",
+		"stats": {
+			"likes": 6,
+			"comments": 72,
+			"visited": 123
+		}
+	}
+]
