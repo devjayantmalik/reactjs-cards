375285bc2a60a646d1263c40264c29c70bc498d1
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-01 11:34:51 +0530

    added data to payload property and fixed spelling mistake warning in reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import {FETCH_POSTS} from './types';
+import posts from '../data/posts';
 
 export const fetchPosts = () => {
   return {
     type: FETCH_POSTS,
-    payload: []
+    payload: posts
   }
 }
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,7 +2,7 @@ import {FETCH_POSTS} from '../actions/types';
 
 export default (posts = [], action) => {
   switch(action.type){
-    case "FETCH_POSTS":
+    case FETCH_POSTS:
       return action.payload
     default:
       return posts;
