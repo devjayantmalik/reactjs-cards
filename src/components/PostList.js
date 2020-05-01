eaf4ea0c7b294c6448576e6e60916fab360e7bab
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-01 11:05:45 +0530

    integrated component with redux

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import Post from './Post';
 
+import {fetchPosts} from '../actions';
+import {connect} from 'react-redux';
+
 class PostList extends React.Component{
+
+  componentDidMount(){
+    this.props.fetchPosts();
+  }
+
+  renderPosts = () => {
+    return (
+        <div className="ui stacked grid container">
+          {this.props.posts.map(post => <Post key={post.id} post={post} />)}
+        </div>
+      );
+
+  }
+
   render(){
+    if(this.props.posts.length) return this.renderPosts();
+
     return(
-      <div className="ui stackable grid container">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+      <div className="ui padded container">
+        <p className="ui red message header">No posts Found.</p>
       </div>
     )
   }
 }
 
-export default PostList;
+const mapStateToProps = (state) => {
+  return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostList);
